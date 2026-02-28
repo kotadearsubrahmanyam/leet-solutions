a1eb5e89d4befea8c2b1f577fd24fc2c3128a205
kotadearsubrahmanyam <kdearsubrahmanyam@gmail.com>
2026-02-28 13:04:56 +0530

    Add submission 1859858269

---
+++ submissions/Strictly Palindromic Number/1859858269.cpp
@@ -0,0 +1,12 @@
+# Title: Strictly Palindromic Number
+# Submission ID: 1859858269
+# Status: Accepted
+# Date: 19 December 2025 at 08:17:27 pm IST
+
+class Solution {
+public:
+    bool isStrictlyPalindromic(int n) {
+        return false;
+        
+    }
+};
